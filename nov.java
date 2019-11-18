41bab78e479c76db230f185b6154b6bee7036735
daryachudo <55081237+daryachudo@users.noreply.github.com>
2019-11-18 10:55:58 +0300

    Add files via upload

---
+++ nov.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class nov {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        double x, y;
+        x=
+        y=Math.atan(double x);
+        System.out.format("Арктангенс %.4f равен %.2f градусам %n",
+
+
+    }
+
+}
